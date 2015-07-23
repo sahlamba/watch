d3d686fe8b63f3b3eaabf30d14aec0716dcb586f
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-23 14:36:10 +0530

    Added django structure, db setup, written models for movie app

---
+++ movie/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('password', models.CharField(max_length=20)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UserData',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('last_active', models.DateTimeField(verbose_name=b'last active')),
+                ('user', models.ForeignKey(to='movie.User')),
+            ],
+        ),
+    ]
