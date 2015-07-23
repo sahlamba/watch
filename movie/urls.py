31b9cb3d4f9f1a5cea74c63d14dc1c7526537c67
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-23 19:43:17 +0530

    Working on search view

--- movie/urls.py
+++ movie/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
+    # Search query example - /movie/search/?query=the%20dark%20knight
+    url(r'^search/$', views.search, name='search'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 def index(request):
     # Render main page template here!
     return HttpResponse("Yo baby!")
+
+def search(request):
+    query = request.GET.get('query')
+    search_results = 
+    return HttpResponse(query)
