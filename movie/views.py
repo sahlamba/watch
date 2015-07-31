96b51be436151eb6bfcd376b8b8c21ed84f7e76a
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-31 12:43:51 +0530

    Added show view and url

--- movie/views.py
+++ movie/views.py
@@ -18,5 +18,11 @@ def search(request):
     search_url = url + 's=' + query + '&type=movie'
     search_results = requests.get(search_url)
     json_results = search_results.json()
-    pdb.set_trace()
     return HttpResponse(json_results['Search'])
+
+def show(request):
+    movie_id = request.GET.get('query')
+    show_url = url + 'i=' + movie_id + '&plot=short&r=json&tomatoes=true'
+    show_data = requests.get(show_url)
+    json_show = show_data.json()
+    return HttpResponse(json_show)
