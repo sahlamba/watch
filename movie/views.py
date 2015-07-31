6f44ba56ebc075f5bcde24887d6952160aacd6fa
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-31 11:47:57 +0530

    Started woking json loading

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+import json
+import urllib
+import requests
+import pdb
+
 from django.http import HttpResponse
 # Create your views here.
+url = 'http://www.omdbapi.com/?t=Game of Thrones&Season=1&Episode=1'
 
 def index(request):
     # Render main page template here!
@@ -9,5 +15,7 @@ def index(request):
 
 def search(request):
     query = request.GET.get('query')
-    search_results = 
-    return HttpResponse(query)
+    search_results = requests.get(url)
+    json_results = search_results.json()
+    pdb.set_trace()
+    return HttpResponse(json_results['Title'])
