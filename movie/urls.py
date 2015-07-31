34dd9d6c45f0b79ce7f6a4f6309afcab39611dae
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-31 12:36:52 +0530

    Added search url

--- movie/urls.py
+++ movie/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name= 'index'),
     # Search query example - /movie/search/?query=the%20dark%20knight
     url(r'^search/$', views.search, name='search'),
+    # Show query example - /movie/show/?query=imdbId
+    url(r'^show/$', views.show, name='show'),
 ]
