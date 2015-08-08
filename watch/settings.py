d5cc61bfc3fa60b77f30b4557ce6a5799834f305
Sahil Lamba <sahil.lamba95@gmail.com>
2015-08-09 00:28:12 +0530

    Fix 2 for whitenoise static file serving

--- watch/settings.py
+++ watch/settings.py
@@ -113,8 +113,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'static/'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
