1c59cc6cbe8c0d806e3d171915d17cb89fe32d3d
Sahil Lamba <sahil.lamba95@gmail.com>
2015-08-09 22:30:12 +0530

    Add search service and function to search box

--- static/scripts/controllers.js
+++ static/scripts/controllers.js
@@ -1,9 +1,26 @@
 // Controllers
 'use strict';
 
-app.controller('MainController', ['$scope',
-  function ($scope) {
+app.controller('MainController', ['$scope', 'Search',
+  function ($scope, Search) {
     $scope.title = 'Popcorn';
+
+    $scope.search = {
+      query: '',
+      results: [],
+      search: function () {
+        if (this.query === undefined || this.query === '') {
+          $scope.search.results = [];
+          return;
+        } else {
+          Search.searchMovie(this.query)
+          .then(function (data) {
+            $scope.search.results = data;
+            console.log($scope.search.results);
+          });
+        }
+      }
+    };
   }
 ]);
 
