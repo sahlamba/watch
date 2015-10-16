03cd8f6bd3ddb238ce30b8a659c08d3441ccabe9
sonali11 <sonali1111gupta@gmail.com>
2015-10-16 22:13:31 +0530

    Initial commit with views

--- movie/views.py
+++ movie/views.py
@@ -4,22 +4,55 @@ import json
 import urllib
 import requests
 import pdb
+from urllib2 import Request, urlopen
 
 from django.http import HttpResponse
 # Create your views here.
-url = 'http://www.omdbapi.com/?'
+#url = 'http://www.omdbapi.com/?'
+
+
+#this hash function receives the name of the file and returns the hash code
+def get_hash(name):
+    readsize = 64 * 1024
+    with open(name, 'rb') as f:
+        size = os.path.getsize(name)
+        data = f.read(readsize)
+        f.seek(-readsize, os.SEEK_END)
+        data += f.read(readsize)
+    return hashlib.md5(data).hexdigest()
+
+# Add as necessary, kids!!
+video_extensions = [".avi",".mp4",".mkv",".mpg",".mpeg",".mov",".rm",".vob",".wmv",".flv",".3gp"]
+
+def remove_ext(filename):
+    # time for some cool coding, bitch!!
+    for ext in video_extensions return filename.replace(ext, "") if (filename.replace(ext, "") != filename)
+    # hash = get_hash(filename)
+
 
 # API views
-def search(request):
-    query = request.GET.get('query')
-    search_url = url + 's=' + query + '&type=movie'
-    search_results = requests.get(search_url)
-    json_results = search_results.json()
-    return HttpResponse(json_results['Search'])
+# def search(request):
+#     query = request.GET.get('query')
+#     search_url = url + 's=' + query + '&type=movie'
+#     search_results = requests.get(search_url)
+#     json_results = search_results.json()
+#     #return HttpResponse(json_results['Search'])
 
 def show(request):
     movie_id = request.GET.get('query')
-    show_url = url + 'i=' + movie_id + '&plot=short&r=json&tomatoes=true'
-    show_data = requests.get(show_url)
-    json_show = show_data.json()
-    return HttpResponse(json_show)
+    omdb_query = url + 'i=' + movie_id + '&plot=full&r=json'
+    omdb_request = Request(omdb_query)
+    response_body = urlopen(omdb_request).read()
+    # Send json response to frontend
+    return HttpResponse(json.dumps(response_body),content_type="application/json")
+
+def subtitles(request):
+    file_name = request.GET.get('query')
+    file_name = remove_ext(file_name)
+    filename_hash = get_hash(file_name)
+    headers = { 'User-Agent' : 'SubDB/1.0 (subtitle-downloader/1.0; http://github.com/amanthedorkknight/subtitleDownloader)' }
+    query_link = "http://api.thesubdb.com/?action=download&hash=" + filename_hash + "&language=en"
+    subDB_query = query_link
+    subDB_request = Request(subDB_query, headers)
+    response_sub = urlopen(subDB_request).read()
+    return HttpResponse(json.dumps(response_sub),content_type="application/json")
