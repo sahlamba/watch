550aec7f583134ad24fa8db181dfaeb244ae6357
sonali11 <sonali1111gupta@gmail.com>
2015-10-16 14:20:29 +0530

    Completed omdb api, Added view for subtitle, TODO: return show json, get subtitle

--- movie/urls.py
+++ movie/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^api/v1/search/$', views.search, name='search'),
     # Show query example - /movie/api/v1/show/?query=imdbId
     url(r'^api/v1/show/$', views.show, name='show'),
+    url(r'^api/v1/sutitles/$', views.subtitles, name='subtitles'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,7 @@ import json
 import urllib
 import requests
 import pdb
+from urllib2 import Request, urlopen
 
 from django.http import HttpResponse
 # Create your views here.
@@ -19,7 +20,31 @@ def search(request):
 
 def show(request):
     movie_id = request.GET.get('query')
-    show_url = url + 'i=' + movie_id + '&plot=short&r=json&tomatoes=true'
-    show_data = requests.get(show_url)
-    json_show = show_data.json()
-    return HttpResponse(json_show)
+    omdb_query = url + 'i=' + movie_id + '&plot=full&r=json'
+    omdb_request = Request(omdb_query)
+    response_body = urlopen(omdb_request).read()
+    # Send json response to frontend
+
+def subtitles(request):
+    file_name = request.GET.get('query')
+    # Store the hash of file_name in movie_name_hash
+    movie_name_hash = get_hash(file_name)
+
+
+#this hash function receives the name of the file and returns the hash code
+def get_hash(name):
+    readsize = 64 * 1024
+    with open(name, 'rb') as f:
+        size = os.path.getsize(name)
+        data = f.read(readsize)
+        f.seek(-readsize, os.SEEK_END)
+        data += f.read(readsize)
+    return hashlib.md5(data).hexdigest()
+
+# Add as necessary, kids!!
+video_extensions = [".avi",".mp4",".mkv",".mpg",".mpeg",".mov",".rm",".vob",".wmv",".flv",".3gp"]
+
+def remove_ext(filename):
+    # time for some cool coding bitch!!
+    for ext in video_extensions return filename.replace(ext, "") if (filename.replace(ext, "") != filename)
+    
